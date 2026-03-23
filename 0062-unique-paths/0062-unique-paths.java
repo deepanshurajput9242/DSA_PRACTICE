951d4d59b3bc74378ae61f19d2ec4faf8aca2a29
deepanshurajput9242 <imdeepanshu1rajpoot@gmail.com>
2026-03-23 22:27:20 +0530

    Time: 0 ms (100%), Space: 42.5 MB (8.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,19 +1,26 @@
+import java.util.*;
+
 class Solution {
     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m][n];
-       return mazePath(0,0,m-1,n-1,dp);
+        int[][] dp = new int[m][n];
 
-        
-    }
-    private static int mazePath(int cr,int cc,int er,int ec,int[][] dp){
-        if(cr==er && cc==ec){
-            return 1;
-
+        for(int i = 0; i < m; i++){
+            Arrays.fill(dp[i], -1);
         }
-        if(cr>er||cc>ec) return 0;
-        if(dp[cr][cc]!=0)return dp[cr][cc];
-        int n1=mazePath(cr+1,cc,er,ec,dp);
-        int n2=mazePath(cr,cc+1,er,ec,dp);
-        return dp[cr][cc]= n1+n2;
+
+        return ways(0, m-1, 0, n-1, dp);
+    }
+
+    public static int ways(int cr, int er, int cc, int ec, int[][] dp){
+        if(cr == er && cc == ec) return 1;
+
+        if(cr > er || cc > ec) return 0;
+
+        if(dp[cr][cc] != -1) return dp[cr][cc];
+
+        int down = ways(cr + 1, er, cc, ec, dp);
+        int right = ways(cr, er, cc + 1, ec, dp);
+
+        return dp[cr][cc] = down + right;
     }
 }
