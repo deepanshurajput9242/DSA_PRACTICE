9b277018a98c9cc3813a8559a2d0062b83d4ca40
deepanshurajput9242 <imdeepanshu1rajpoot@gmail.com>
2026-04-03 23:54:24 +0530

    Time: 2 ms (99.42%), Space: 119.6 MB (33.24%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,22 +1,53 @@
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int totalcost=0;
-        int totalgas=0;
-        for(int i=0;i<gas.length;i++){
-            totalgas+=gas[i];
-            totalcost+=cost[i];
+
+        int n=gas.length;
+        int total=0;
+        int result=0;
+        for(int i=0;i<n;i++){
+            total+=gas[i]-cost[i];
         }
-        if(totalcost>totalgas)return -1;
-        int remaingas=0;
-        int start=0;
-        for(int i=0;i<gas.length;i++){
-            remaingas=remaingas+(gas[i]-cost[i]);
-            if(remaingas<0){
-                start=i+1;
-                remaingas=0;
+        if(total<0)return -1;
+        total=0;
+        for(int i=0;i<n;i++){
+            total+=gas[i]-cost[i];
+            if(total<0){
+                total=0;
+                result=i+1;
             }
+           
         }
-        return start;
+        return result;
+      
         
     }
 }
+
+
+
+
+
+  // int curGas=0;
+        // int n=gas.length;
+        // for(int i=0;i<n;i++){
+        //     if(gas[i]<cost[i]){continue;
+        //     }
+        //     int j=(i+1)%n;
+        //     curGas=gas[i]-cost[i]+gas[j];
+        //     while(j!=i){
+        //         if(curGas<cost[j]){
+        //             break;
+        //         }
+        //         int costForMovingThisJ=cost[j];
+        //         j=(j+1)%n;
+        //         int gasEarnInNextStationJ=gas[j];
+        //         curGas=curGas-costForMovingThisJ+gasEarnInNextStationJ;
+                
+
+        //     }
+        //     if(j==i){
+        //         return i;
+        //     }
+
+        // }
+        // return -1;
