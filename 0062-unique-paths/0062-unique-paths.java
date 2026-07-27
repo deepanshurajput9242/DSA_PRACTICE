9db3bb070840dbec9e62310689ff3bf450840660
deepanshurajput9242 <imdeepanshu1rajpoot@gmail.com>
2026-07-27 15:14:24 +0530

    Time: 0 ms (100%), Space: 42.1 MB (66.58%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -22,37 +22,3 @@ class Solution {
         
     }
 }
-
-
-
-
-
-
-
- // public int uniquePaths(int m, int n) {
-    //     int[][] dp = new int[m][n];
-    //     for(int i=0;i<m;i++){
-    //         for(int j=0;j<n;j++){
-    //             if(i==0 || j==0) dp[i][j]=1;
-    //             else {dp[i][j] = dp[i][j-1]+dp[i-1][j];}
-    //         }
-    //     }
-    //     return dp[m-1][n-1];
-//  for(int i = 0; i < m; i++){
-    //         Arrays.fill(dp[i], -1);
-    //     }
-
-    //     return ways(0, m-1, 0, n-1, dp);
-    // }
-
-    // public static int ways(int cr, int er, int cc, int ec, int[][] dp){
-    //     if(cr == er && cc == ec) return 1;
-
-    //     if(cr > er || cc > ec) return 0;
-
-    //     if(dp[cr][cc] != -1) return dp[cr][cc];
-
-    //     int down = ways(cr + 1, er, cc, ec, dp);
-    //     int right = ways(cr, er, cc + 1, ec, dp);
-
-    //     return dp[cr][cc] = down + right;
