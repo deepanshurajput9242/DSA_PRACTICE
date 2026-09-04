f57f17b5dc0610c0c70b85204207c0f68a8ea7de
deepanshurajput9242 <imdeepanshu1rajpoot@gmail.com>
2026-09-04 21:46:37 +0530

    Time: 2 ms (99.18%), Space: 46.6 MB (52.52%) - LeetHub

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.java
@@ -1,27 +1,46 @@
 class Solution {
     public int[] leftRightDifference(int[] nums) {
-        int[] answer=new int[nums.length];
-        int[] leftSum=new int[nums.length];
-        int[] rightSum=new int[nums.length];
-        leftSum[0]=0;
-        rightSum[nums.length-1]=0;
-        int leftSumo=0;
-        int rightSumo=0;
+        // int[] answer=new int[nums.length];
+        // int[] leftSum=new int[nums.length];
+        // int[] rightSum=new int[nums.length];
+        // leftSum[0]=0;
+        // rightSum[nums.length-1]=0;
+        // int leftSumo=0;
+        // int rightSumo=0;
 
-        for(int i=1;i<nums.length;i++){
+        // for(int i=1;i<nums.length;i++){
 
-            leftSumo+=nums[i-1];
-            leftSum[i]=leftSumo;
+        //     leftSumo+=nums[i-1];
+        //     leftSum[i]=leftSumo;
 
-        }
-        for(int i=nums.length-2;i>=0;i--){
-            rightSumo+=nums[i+1];
-            rightSum[i]=rightSumo;
-        }
-        for(int i=0;i<nums.length;i++){
-            answer[i]=Math.abs(leftSum[i]-rightSum[i]);
-        }
-        return answer;
+        // }
+        // for(int i=nums.length-2;i>=0;i--){
+        //     rightSumo+=nums[i+1];
+        //     rightSum[i]=rightSumo;
+        // }
+        // for(int i=0;i<nums.length;i++){
+        //     answer[i]=Math.abs(leftSum[i]-rightSum[i]);
+        // }
+     int[] ans=new int[nums.length];
+     int total=0;
+     for(int i=0;i<nums.length;i++){
+        total+=nums[i];
+     }
+     int leftSum=0;
+     for(int i=0;i<nums.length;i++ ){
+        int rightSum=total-leftSum-nums[i];
+        ans[i]=Math.abs(rightSum-leftSum);
+        leftSum+=nums[i];
+     }
+     
+
+
+
+
+
+
+        
+        return ans;
         
     }
 }
